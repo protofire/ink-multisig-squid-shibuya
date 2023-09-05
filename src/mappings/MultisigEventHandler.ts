b55fe64775a2e08002c5e3c79581c853a97ec0e5
0xLucca <95830307+0xLucca@users.noreply.github.com>
2023-09-05 11:02:54 -0300

    Remove duplicated code (#6)

--- src/mappings/MultisigEventHandler.ts
+++ src/mappings/MultisigEventHandler.ts
@@ -89,12 +89,7 @@ export class MultisigEventHandler {
     event: multisig.Event_ThresholdChanged,
     blockHeader: SubstrateBlock
   ) {
-    // If multisigData doesn't have this multisig, it means that we need to fetch it from the DB
-    if (!existingMultisigs.get(contractAddressHex)) {
-      await this.fetchMultisigDataFromDB(contractAddressHex);
-    }
-
-    // Update the threshold
+    await this.fetchMultisigDataFromDBIfNeeded(contractAddressHex);
     multisigData[contractAddressHex].threshold = event.threshold;
   }
 
@@ -103,12 +98,7 @@ export class MultisigEventHandler {
     event: multisig.Event_OwnerAdded,
     blockHeader: SubstrateBlock
   ) {
-    // If multisigData doesn't have this multisig, it means that we need to fetch it from the DB
-    if (!existingMultisigs.get(contractAddressHex)) {
-      await this.fetchMultisigDataFromDB(contractAddressHex);
-    }
-
-    // Add the new owner
+    await this.fetchMultisigDataFromDBIfNeeded(contractAddressHex);
     multisigData[contractAddressHex].owners.push(
       ss58.codec(SS58_PREFIX).encode(event.owner)
     );
@@ -119,12 +109,7 @@ export class MultisigEventHandler {
     event: multisig.Event_OwnerRemoved,
     blockHeader: SubstrateBlock
   ) {
-    // If multisigData doesn't have this multisig, it means that we need to fetch it from the DB
-    if (!existingMultisigs.get(contractAddressHex)) {
-      await this.fetchMultisigDataFromDB(contractAddressHex);
-    }
-
-    // Remove the owner
+    await this.fetchMultisigDataFromDBIfNeeded(contractAddressHex);
     multisigData[contractAddressHex].owners = multisigData[
       contractAddressHex
     ].owners.filter(
@@ -137,39 +122,25 @@ export class MultisigEventHandler {
     event: multisig.Event_TransactionProposed,
     blockHeader: SubstrateBlock
   ) {
-    //Each time a transaction is proposed, we create a new transaction record and also a new approval record for the creator
-    const newTransactionId = contractAddressHex + "-" + event.txId;
+    const newTransactionId = this.createTransactionId(contractAddressHex, event.txId);
     existingTransactions.add(newTransactionId);
-    transactionData[newTransactionId] = {
-      id: newTransactionId,
-      multisig: contractAddressHex,
-      txId: event.txId,
-      proposer: ss58.codec(SS58_PREFIX).encode(event.proposer),
-      contractAddress: ss58.codec(SS58_PREFIX).encode(event.contractAddress),
-      selector: uint8ArrayToHexString(event.selector),
-      args: uint8ArrayToHexString(event.input),
-      value: event.transferredValue,
-      status: TransactionStatus.PROPOSED,
-      error: "",
-      approvalCount: 1,
-      rejectionCount: 0,
-      lastUpdatedTimestamp: new Date(blockHeader.timestamp),
-      lastUpdatedBlockNumber: blockHeader.height,
-    };
+    transactionData[newTransactionId] = this.createTransactionData(
+      newTransactionId,
+      contractAddressHex,
+      event,
+      blockHeader,
+      TransactionStatus.PROPOSED
+    );
 
     const newApprovalId =
       newTransactionId + "-" + uint8ArrayToHexString(event.proposer);
 
-    // Add the approval
-    const newApproval: ApprovalOrRejectionRecord = {
-      id: newApprovalId,
-      transaction: newTransactionId,
-      caller: ss58.codec(SS58_PREFIX).encode(event.proposer),
-      timestamp: new Date(blockHeader.timestamp),
-      blockNumber: blockHeader.height,
-    };
-
-    approvals.push(newApproval);
+    approvals.push(this.createApprovalOrRejectionRecord(
+      newApprovalId,
+      newTransactionId,
+      event.proposer,
+      blockHeader
+    ));
   }
 
   private async handleTransactionExecuted(
@@ -177,56 +148,17 @@ export class MultisigEventHandler {
     event: multisig.Event_TransactionExecuted,
     blockHeader: SubstrateBlock
   ) {
-    const transactionId = contractAddressHex + "-" + event.txId;
-    if (!existingTransactions.has(transactionId)) {
-      let dbTx = (
-        await this.transactionRepository.findById([transactionId])
-      )[0]; //This must exist and be unique
-      transactionData[transactionId] = {
-        id: transactionId,
-        multisig: contractAddressHex,
-        txId: event.txId,
-        proposer: dbTx.proposer,
-        contractAddress: dbTx.contractAddress,
-        selector: dbTx.selector,
-        args: dbTx.args,
-        value: dbTx.value,
-        status: dbTx.status,
-        error: dbTx.error,
-        approvalCount: dbTx.approvalCount,
-        rejectionCount: dbTx.rejectionCount,
-        lastUpdatedTimestamp: dbTx.lastUpdatedTimestamp,
-        lastUpdatedBlockNumber: dbTx.lastUpdatedBlockNumber,
-      };
+    const transactionId = this.createTransactionId(contractAddressHex, event.txId);
+    await this.fetchTransactionDataFromDBIfNeeded(transactionId);
 
-      existingTransactions.add(transactionId);
-    }
-
-    // Update the transaction
-    transactionData[transactionId].lastUpdatedTimestamp = new Date(
-      blockHeader.timestamp
+    this.updateTransactionData(
+      transactionId,
+      blockHeader,
+      event.result.__kind === "Success" ? TransactionStatus.EXECUTED_SUCCESS : TransactionStatus.EXECUTED_FAILURE
     );
-    transactionData[transactionId].lastUpdatedBlockNumber = blockHeader.height;
 
-    if (event.result.__kind === "Success") {
-      transactionData[transactionId].status =
-        TransactionStatus.EXECUTED_SUCCESS;
-    }
     if (event.result.__kind === "Failed") {
-      transactionData[transactionId].status =
-        TransactionStatus.EXECUTED_FAILURE;
-
-      if (event.result.value.__kind === "EnvExecutionFailed") {
-        let error = event.result.value as MultisigError_EnvExecutionFailed;
-        transactionData[transactionId].error =
-          event.result.value.__kind + ": " + error.value;
-      } else if (event.result.value.__kind === "LangExecutionFailed") {
-        let error = event.result.value as MultisigError_LangExecutionFailed;
-        transactionData[transactionId].error =
-          event.result.value.__kind + ": " + error.value.__kind;
-      } else {
-        transactionData[transactionId].error = event.result.value.__kind;
-      }
+      transactionData[transactionId].error = this.getErrorMessage(event.result);
     }
   }
 
@@ -235,37 +167,14 @@ export class MultisigEventHandler {
     event: multisig.Event_TransactionCancelled,
     blockHeader: SubstrateBlock
   ) {
-    const transactionId = contractAddressHex + "-" + event.txId;
-    if (!existingTransactions.has(transactionId)) {
-      let dbTx = (
-        await this.transactionRepository.findById([transactionId])
-      )[0]; //This must exist and be unique
-      transactionData[transactionId] = {
-        id: transactionId,
-        multisig: contractAddressHex,
-        txId: event.txId,
-        proposer: dbTx.proposer,
-        contractAddress: dbTx.contractAddress,
-        selector: dbTx.selector,
-        args: dbTx.args,
-        value: dbTx.value,
-        status: dbTx.status,
-        error: dbTx.error,
-        approvalCount: dbTx.approvalCount,
-        rejectionCount: dbTx.rejectionCount,
-        lastUpdatedTimestamp: dbTx.lastUpdatedTimestamp,
-        lastUpdatedBlockNumber: dbTx.lastUpdatedBlockNumber,
-      };
+    const transactionId = this.createTransactionId(contractAddressHex, event.txId);
+    await this.fetchTransactionDataFromDBIfNeeded(transactionId);
 
-      existingTransactions.add(transactionId);
-    }
-
-    // Update the transaction
-    transactionData[transactionId].lastUpdatedTimestamp = new Date(
-      blockHeader.timestamp
+    this.updateTransactionData(
+      transactionId,
+      blockHeader,
+      TransactionStatus.CANCELLED
     );
-    transactionData[transactionId].lastUpdatedBlockNumber = blockHeader.height;
-    transactionData[transactionId].status = TransactionStatus.CANCELLED;
   }
 
   private async handleApprove(
@@ -273,45 +182,20 @@ export class MultisigEventHandler {
     event: multisig.Event_Approve,
     blockHeader: SubstrateBlock
   ) {
-    const transactionId = contractAddressHex + "-" + event.txId;
-    const newApprovalId =
-      transactionId + "-" + uint8ArrayToHexString(event.owner);
-    // Check if the tx is on memory. If not, fetch it from the DB
-    if (!existingTransactions.has(transactionId)) {
-      let dbTx = (await this.transactionRepository.findById([transactionId]))[0]; //This must exist and be unique
-      transactionData[transactionId] = {
-        id: transactionId,
-        multisig: contractAddressHex,
-        txId: dbTx.txId,
-        proposer: dbTx.proposer,
-        contractAddress: dbTx.contractAddress,
-        selector: dbTx.selector,
-        args: dbTx.args,
-        value: dbTx.value,
-        status: dbTx.status,
-        error: dbTx.error,
-        approvalCount: dbTx.approvalCount,
-        rejectionCount: dbTx.rejectionCount,
-        lastUpdatedTimestamp: dbTx.lastUpdatedTimestamp,
-        lastUpdatedBlockNumber: dbTx.lastUpdatedBlockNumber,
-      };
+    const transactionId = this.createTransactionId(contractAddressHex, event.txId);
+    await this.fetchTransactionDataFromDBIfNeeded(transactionId);
 
-      existingTransactions.add(transactionId);
-    }
-
-    // Update the transaction
     transactionData[transactionId].approvalCount += 1;
 
-    // Add the approval
-    const newApproval: ApprovalOrRejectionRecord = {
-      id: newApprovalId,
-      transaction: transactionId,
-      caller: ss58.codec(SS58_PREFIX).encode(event.owner),
-      timestamp: new Date(blockHeader.timestamp),
-      blockNumber:blockHeader.height,
-    };
+    const newApprovalId =
+      transactionId + "-" + uint8ArrayToHexString(event.owner);
 
-    approvals.push(newApproval);
+    approvals.push(this.createApprovalOrRejectionRecord(
+      newApprovalId,
+      transactionId,
+      event.owner,
+      blockHeader
+    ));
   }
 
   private async handleReject(
@@ -319,55 +203,112 @@ export class MultisigEventHandler {
     event: multisig.Event_Reject,
     blockHeader: SubstrateBlock
   ) {
-    const transactionId = contractAddressHex + "-" + event.txId;
-            const newRejectionId =
-              transactionId + "-" + uint8ArrayToHexString(event.owner);
-            // Check if the tx is on memory. If not, fetch it from the DB
-            if (!existingTransactions.has(transactionId)) {
-              let dbTx = (
-                await this.transactionRepository.findById([transactionId])
-              )[0]; //This must exist and be unique
-              transactionData[transactionId] = {
-                id: transactionId,
-                multisig: contractAddressHex,
-                txId: dbTx.txId,
-                proposer: dbTx.proposer,
-                contractAddress: dbTx.contractAddress,
-                selector: dbTx.selector,
-                args: dbTx.args,
-                value: dbTx.value,
-                status: dbTx.status,
-                error: dbTx.error,
-                approvalCount: dbTx.approvalCount,
-                rejectionCount: dbTx.rejectionCount,
-                lastUpdatedTimestamp: dbTx.lastUpdatedTimestamp,
-                lastUpdatedBlockNumber: dbTx.lastUpdatedBlockNumber,
-              };
+    const transactionId = this.createTransactionId(contractAddressHex, event.txId);
+    await this.fetchTransactionDataFromDBIfNeeded(transactionId);
 
-              existingTransactions.add(transactionId);
-            }
+    transactionData[transactionId].rejectionCount += 1;
 
-            // Update the transaction
-            transactionData[transactionId].rejectionCount += 1;
+    const newRejectionId =
+      transactionId + "-" + uint8ArrayToHexString(event.owner);
 
-            // Add the approval
-            const newRejection: ApprovalOrRejectionRecord = {
-              id: newRejectionId,
-              transaction: transactionId,
-              caller: ss58.codec(SS58_PREFIX).encode(event.owner),
-              timestamp: new Date(blockHeader.timestamp),
-              blockNumber: blockHeader.height,
-            };
-
-            rejections.push(newRejection);
+    rejections.push(this.createApprovalOrRejectionRecord(
+      newRejectionId,
+      transactionId,
+      event.owner,
+      blockHeader
+    ));
   }
 
-  private async fetchMultisigDataFromDB(contractAddressHex: string) {
-    multisigData[contractAddressHex] = (
-      await this.multisigRepository.findByAddressHex([contractAddressHex])
-    )[0]; //This must exist and be unique
+  private async fetchMultisigDataFromDBIfNeeded(contractAddressHex: string) {
+    if (!existingMultisigs.get(contractAddressHex)) {
+      multisigData[contractAddressHex] = (
+        await this.multisigRepository.findByAddressHex([contractAddressHex])
+      )[0]; //This must exist and be unique
 
-    // Set map to true to indicate that we have the content on multisigData
-    existingMultisigs.set(contractAddressHex, true);
+      // Set map to true to indicate that we have the content on multisigData
+      existingMultisigs.set(contractAddressHex, true);
+    }
+  }
+
+  private createTransactionId(contractAddressHex: string, txId: bigint) {
+    return contractAddressHex + "-" + txId;
+  }
+
+  private async fetchTransactionDataFromDBIfNeeded(transactionId: string) {
+    if (!existingTransactions.has(transactionId)) {
+      let dbTx = (
+        await this.transactionRepository.findById([transactionId])
+      )[0]; //This must exist and be unique
+      transactionData[transactionId] = {...dbTx,
+        multisig: dbTx.multisig.addressHex
+      };
+      existingTransactions.add(transactionId);
+    }
+  }
+
+  private createTransactionData(
+    newTransactionId: string,
+    contractAddressHex: string,
+    event: multisig.Event_TransactionProposed,
+    blockHeader: SubstrateBlock,
+    status: TransactionStatus
+  ) {
+    return {
+      id: newTransactionId,
+      multisig: contractAddressHex,
+      txId: event.txId,
+      proposer: ss58.codec(SS58_PREFIX).encode(event.proposer),
+      contractAddress: ss58.codec(SS58_PREFIX).encode(event.contractAddress),
+      selector: uint8ArrayToHexString(event.selector),
+      args: uint8ArrayToHexString(event.input),
+      value: event.transferredValue,
+      status: status,
+      error: "",
+      approvalCount: 1,
+      rejectionCount: 0,
+      lastUpdatedTimestamp: new Date(blockHeader.timestamp),
+      lastUpdatedBlockNumber: blockHeader.height,
+    };
+  }
+
+  private updateTransactionData(
+    transactionId: string,
+    blockHeader: SubstrateBlock,
+    status: TransactionStatus
+  ) {
+    transactionData[transactionId].lastUpdatedTimestamp = new Date(
+      blockHeader.timestamp
+    );
+    transactionData[transactionId].lastUpdatedBlockNumber = blockHeader.height;
+    transactionData[transactionId].status = status;
+  }
+
+  private getErrorMessage(result: multisig.Event_TransactionExecuted["result"]) {
+    if('__kind' in result.value){
+      if (result.value.__kind === "EnvExecutionFailed") {
+        let error = result.value as MultisigError_EnvExecutionFailed;
+        return result.value.__kind + ": " + error.value;
+      } else if (result.value.__kind === "LangExecutionFailed") {
+        let error = result.value as MultisigError_LangExecutionFailed;
+        return result.value.__kind + ": " + error.value.__kind;
+      } else {
+        return result.value.__kind;
+      }
+    }
+  }
+
+  private createApprovalOrRejectionRecord(
+    id: string,
+    transactionId: string,
+    owner: Uint8Array,
+    blockHeader: SubstrateBlock
+  ) {
+    return {
+      id: id,
+      transaction: transactionId,
+      caller: ss58.codec(SS58_PREFIX).encode(owner),
+      timestamp: new Date(blockHeader.timestamp),
+      blockNumber: blockHeader.height,
+    };
   }
 }
